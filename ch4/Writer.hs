cb1937e61b58805bae5d5f0c27ad205e23554d8b
Farooq <farooq1.khan@ryerson.ca>
2022-06-04 18:09:08 -0400

    wrote writer class

--- ch4/Writer.hs
+++ ch4/Writer.hs
@@ -1,9 +1,11 @@
+import Data.Char
 type Writer a = (a, String)
 
 (>=>) :: (a -> Writer b) -> (b -> Writer c) -> (a -> Writer c)
 m1 >=> m2 = \x -> 
-        let (y, s1) = m1 x 
-        let (z, s2) = m2 y 
+        let 
+                (y, s1) = m1 x 
+                (z, s2) = m2 y 
         in (z, s1 ++ s2) 
 
 return :: a -> Writer a
@@ -13,7 +15,7 @@ upCase :: String -> Writer String
 upCase s = (map toUpper s, "upCase ")
 
 toWords :: String -> Writer [String]
-toWords s = (word s, "toWords ")
+toWords s = (words s, "toWords ")
 
 process :: String -> Writer [String]
 process = upCase >=> toWords 
